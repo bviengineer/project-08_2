ac7439c7e568bcaac42c5722a6e318e1dbe5da0c
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-03-01 23:30:21 -0500

    Update doLogin.php
    -Update session amed for user id to 'auth_user_id

--- procedures/doLogin.php
+++ procedures/doLogin.php
@@ -16,7 +16,7 @@ if (!password_verify(request()->get('password'), $user['password'])) {
 
 //
 $session->set('auth_logged_in', true);
-$session->set('auth_logged_id', (int) $user['id']);
+$session->set('auth_user_id', (int) $user['id']);
 
 $session->getFlashBag()->add('success', "Successfully Logged In"); 
 redirect('/');
