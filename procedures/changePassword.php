c5a124214ece53580aa6aab3dededd1b63105b2e
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-03-02 08:44:37 -0500

    Update changePassword.php
    -Update variable name to $newPwd
    -Call function to get authenticated user
    -Add error messagaes if user could not be found or password could not be updated
    -Add success message if password update was successful

--- procedures/changePassword.php
+++ procedures/changePassword.php
@@ -3,12 +3,40 @@ require __DIR__.'/../inc/bootstrap.php';
 requireAuth();
 
 $currentPwd = request()->get('current_password');
-$newPassword = request()->get('password');
+$newPwd = request()->get('password');
 $confirmedPwd = request()->get('confirm_password');
 
 
 // Verifies whether new password matches the confirmed version of the new password
-if ($newPassword != $confirmedPassword) {
+if ($newPwd != $confirmedPwd) {
     $session->getFlashBag()->add('error', 'New passwords do not match, please try again');
     redirect('/account.php');
 }
+
+// Get the user attempting to change their password
+$user = getAuthenticatedUser();
+
+// If a username could not be found 
+if (empty($user)) {
+    $session->getFlashBag()->add('error', 'Some error Happened. Try again.  If it continues, please 
+    log out and log back in.');
+    redirect('/account.php');
+}
+
+// Verifies whether the password in the database matches the user supplied password
+if (!password_verify($currentPwd, $user['password'])) {
+    $session->getFlashBag()->add('error', 'Current password is incorrect. Please try again');
+    redirect('/account.php');
+}
+
+// Hashes the updated password
+$updatedPwd = updatePassword(password_hash($newPwd, PASSWORD_DEFAULT), $user['username']);
+
+// If password could not be updated
+if (!$updatedPwd) {
+    $session->getFlashBag()->add('error', 'Could not update password. Please try again.');
+    redirect('/account.php');
+}
+// Displays success message if the password was updated successfully 
+$session->getFlashBag()->add('success', 'Password updated');
+redirect('/account.php');
