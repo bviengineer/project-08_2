6de4f00e2576d2f5c20556f392266ae27e74b7a1
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-03-01 21:46:15 -0500

    Update functions_users.php
    Add function to find user by username in the database

--- inc/functions_users.php
+++ inc/functions_users.php
@@ -1,1 +1,15 @@
 <?php
+
+// Find a user by username
+function findUserByUsername($username) {
+    global $db;
+    try {
+        $query = $db->prepare('SELECT * from users where username = :username');
+        $query->bindParam(':username', $username);
+        $query->execute();
+        $result = $query->fetch(PDO::FETCH_ASSOC);
+        return $result;
+    } catch (\Exception $e) {
+        throw $e;
+    }
+}
