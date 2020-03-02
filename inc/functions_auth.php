528cff9b54d95e618b61580f56f853b9a1fd81ed
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-03-01 23:41:31 -0500

    Update functions_auth.php
    Create function to automatically save user data and set their status to logged in

--- inc/functions_auth.php
+++ inc/functions_auth.php
@@ -4,3 +4,11 @@ function isAuthenticated() {
     global $session;
     return $session->get('auth_logged_in', false);
 }
+
+function saveUserData($user) {
+    global $session;
+    $session->set('auth_logged_in', true);
+    $session->set('auth_user_id', (int) $user['id']);
+    
+    $session->getFlashBag()->add('success', "Successfully Logged In"); 
+}
