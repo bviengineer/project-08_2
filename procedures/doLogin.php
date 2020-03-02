7d3b49c1d0db65d22c24ca25c2f0b0694c5e4e6a
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-03-01 23:03:40 -0500

    Update doLogin.php
    Add code to verify whether the password matches

--- procedures/doLogin.php
+++ procedures/doLogin.php
@@ -7,3 +7,9 @@ if (empty($user)) {
     $session->getFlashBag()->add('error', 'Username was not found');
     redirect('/login.php');
 }
+
+// Verfies whether the provided password matches the stored hashed password
+if (!password_verify(request()->get('password'), $user['password'])) {
+    $session->getFlashBag()->add('error', 'Your uername or password is incorrect. Please try again.');
+    redirect('/login.php');
+}
