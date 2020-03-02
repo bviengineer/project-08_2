15fbd103a2cb3eed878c95f1382c079d0f8ea4a3
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-03-02 18:50:26 -0500

    functions_auth.php & doLogout.php
    Comment out sessions that are not longer needed

--- inc/functions_auth.php
+++ inc/functions_auth.php
@@ -25,8 +25,8 @@ function getAuthenticatedUser() {
 // Save user session data
 function saveUserData($user) {
     global $session;
-    $session->set('auth_logged_in', true);
-    $session->set('auth_user_id', (int) $user['id']);
+    // $session->set('auth_logged_in', true);
+    // $session->set('auth_user_id', (int) $user['id']);
     
     $session->getFlashBag()->add('success', "Successfully Logged In"); 
     $data = ['auth_user_id' => (int) $user['id'] ];
--- procedures/doLogout.php
+++ procedures/doLogout.php
@@ -1,8 +1,8 @@
 <?php
 require __DIR__.'/../inc/bootstrap.php';
 
-$session->remove('auth_logged_in');
-$session->remove('auth_user_id');
+// $session->remove('auth_logged_in');
+// $session->remove('auth_user_id');
 
 // Log out confirmation message
 $session->getFlashBag()->add('success', "Successfully Logged Out");
