5d17c9a77f9cc0c5aaa027a3fb8ee7fb94a6add1
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-03-01 23:03:14 -0500

    Update doLogin.php
    Add code to verify if the the username already exist

--- procedures/doLogin.php
+++ procedures/doLogin.php
@@ -1,2 +1,9 @@
 <?php
 require __DIR__.'/../inc/bootstrap.php';
+
+// Verifies that the user entered a username
+$user = findUserByUsername(request()->get('username'));
+if (empty($user)) {
+    $session->getFlashBag()->add('error', 'Username was not found');
+    redirect('/login.php');
+}
