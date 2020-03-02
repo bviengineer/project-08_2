6bea0ef75d3988f2607b624089d1919ac4663911
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-03-02 13:52:48 -0500

    Update index.php
    Update date index page to show Login and Register when users are not logged in

--- index.php
+++ index.php
@@ -32,29 +32,21 @@ include 'inc/header.php';
   <?php else: ?>
       <ul class="actions">
           <li class="actions-item">
-            <a class="actions-link" href="register.php">
-              <span class="actions-icon">
-                <svg viewbox="0 0 64 64"><use xlink:href="#user_icon"></use></svg>
-              </span>
-                Register
-              </a>
-          </li>
-          <li class="actions-item">
-                <a class="actions-link" href="task_list.php">
+            <a class="actions-link" href="/login.php">
               <span class="actions-icon">
                 <svg viewbox="0 0 64 64"><use xlink:href="#report_icon"></use></svg>
               </span>
-                    View Tasks
+                Login
+              </a>
+          </li>
+          <li class="actions-item">
+                <a class="actions-link" href="register.php">
+              <span class="actions-icon">
+                <svg viewbox="0 0 64 64"><use xlink:href="#user_icon"></use></svg>
+              </span>
+                    Register
                 </a>
             </li>
-          <li class="actions-item">
-            <a class="actions-link" href="task.php">
-              <span class="actions-icon">
-                <svg viewbox="0 0 64 64"><use xlink:href="#task_icon"></use></svg>
-              </span>
-              Add Task
-            </a>
-          </li>
         <?php endif; ?>
 		</div>
 
