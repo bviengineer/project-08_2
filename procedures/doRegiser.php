d085f27b58ff187943c64e36b0e80b11ea63c22d
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-03-01 21:37:51 -0500

    Update doRegiser.php
    Add code to verify whether passwords match upon registration attempt

--- procedures/doRegiser.php
+++ procedures/doRegiser.php
@@ -1,1 +1,12 @@
 <?php
+require_once __DIR__ . '/../inc/bootstrap.php';
+
+$username->get('username');
+$password->get('password');
+$confirmedPwd->get('confirm_password');
+
+// Verifies whether passwords match
+if ($password != $confirmedPwd) {
+    $session->getFlashBag()->add('error', 'Passwords do NOT match');
+    redirect('/register.php');
+}
