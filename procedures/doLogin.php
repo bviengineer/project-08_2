32cc2a5514f33bf1eae561ccf5361cd9a679842a
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-03-01 23:42:12 -0500

    Update doLogin.php
    -Remove sessions setting the logged in status and user id
    -Call to function to save user data and log in registrant

--- procedures/doLogin.php
+++ procedures/doLogin.php
@@ -14,9 +14,5 @@ if (!password_verify(request()->get('password'), $user['password'])) {
     redirect('/login.php');
 }
 
-//
-$session->set('auth_logged_in', true);
-$session->set('auth_user_id', (int) $user['id']);
-
-$session->getFlashBag()->add('success', "Successfully Logged In"); 
+saveUserData($user);
 redirect('/');
