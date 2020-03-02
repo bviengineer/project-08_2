addc4631bcea0e5adcb01c2f211b64e19de7faef
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-03-01 23:35:09 -0500

    Update doLogout.php
    -Add code to log user out

--- procedures/doLogout.php
+++ procedures/doLogout.php
@@ -1,2 +1,9 @@
 <?php
 require __DIR__.'/../inc/bootstrap.php';
+
+$session->remove('auth_logged_in');
+$session->remove('auth_user_id');
+
+// Log out confirmation message
+$session->getFlashBag()->add('success', "Successfully Logged Out");
+redirect('/login.php');
