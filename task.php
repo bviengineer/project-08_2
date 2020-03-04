29ed340f06b902a230d97bd4939463f36299ab11
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-03-04 13:37:24 -0500

    Update task.php
    Add conditional before echoing the value of the task

--- task.php
+++ task.php
@@ -31,7 +31,7 @@ include 'inc/header.php';
                 <table>
                     <tr>
                         <th><label for="task">Task<span class="required">*</span></label></th>
-                        <td><input type="text" id="task" name="task" value="<?php echo htmlspecialchars($task); ?>" /></td>
+                        <td><input type="text" id="task" name="task" value="<?php if(isset($task)) {echo htmlspecialchars($task); } ?>" /></td>
                     </tr>
                    </table>
                 <?php
