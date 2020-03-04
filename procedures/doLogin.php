bed62218b13cdd6b727fb54660a00ac6cd712c40
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-03-04 13:50:40 -0500

    Update doLogin.php
    -Remove white space
    -Remove commented out code

--- procedures/doLogin.php
+++ procedures/doLogin.php
@@ -7,12 +7,9 @@ if (empty($user)) {
     $session->getFlashBag()->add('error', 'Username was not found');
     redirect('/login.php');
 }
-
 // Verfies whether the provided password matches the stored hashed password
 if (!password_verify(request()->get('password'), $user['password'])) {
     $session->getFlashBag()->add('error', 'Username or password is incorrect. Please try again.');
     redirect('/login.php');
 }
-
 saveUserData($user);
-// redirect('/');
